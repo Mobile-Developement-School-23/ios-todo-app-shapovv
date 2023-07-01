60c6f7e11ebc7b0f42b07139b3d26e6203138629
shapovv <asshapovalov@yandex.ru>
2023-07-01 06:57:59 +0300

    HW_1

--- todoTests/todoTests.swift
+++ todoTests/todoTests.swift
@@ -1,36 +1,113 @@
-//
-//  todoTests.swift
-//  todoTests
-//
-//  Created by Артём Шаповалов on 01.07.2023.
-//
-
 import XCTest
 @testable import todo
 
-final class todoTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class ToDoTests: XCTestCase {
+    
+    func testInitTodoItem() {
+        let todoItem = TodoItem(text: "Do homework 1 in SHMR", importance: .important, isDone: false)
+        
+        XCTAssertNotNil(todoItem)
+        XCTAssertEqual(todoItem.text, "Do homework 1 in SHMR")
+        XCTAssertEqual(todoItem.importance, .important)
+        XCTAssertFalse(todoItem.isDone)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+    func testParseJson() {
+        let json: [String: Any] = ["id": "1",
+                                   "text": "Do homework 1 in SHMR",
+                                   "importance": "important",
+                                   "isDone": false,
+                                   "creationDate": ISO8601DateFormatter().string(from: Date())]
+
+        let todoItem = TodoItem.parse(json: json)
+        
+        XCTAssertNotNil(todoItem)
+        XCTAssertEqual(todoItem?.id, "1")
+        XCTAssertEqual(todoItem?.text, "Do homework 1 in SHMR")
+        XCTAssertEqual(todoItem?.importance, .important)
+        XCTAssertFalse(todoItem!.isDone)
+    }
+    
+
+
+    func testToJson() {
+        let todoItem = TodoItem(text: "Do homework 1 in SHMR", importance: .important, isDone: false)
+        let json = todoItem.json as? [String: Any]
+        
+        XCTAssertNotNil(json)
+        XCTAssertEqual(json?["text"] as? String, "Do homework 1 in SHMR")
+        XCTAssertEqual(json?["importance"] as? String, "important")
+        XCTAssertEqual(json?["isDone"] as? Bool, false)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
+    func testParseCsv() {
+        let csv = "1,Do homework 1 in SHMR,false,\(ISO8601DateFormatter().string(from: Date())),important"
+
+        let todoItem = TodoItem.parse(csv: csv)
+        
+        XCTAssertNotNil(todoItem)
+        XCTAssertEqual(todoItem?.id, "1")
+        XCTAssertEqual(todoItem?.text, "Do homework 1 in SHMR")
+        XCTAssertEqual(todoItem?.importance, .important)
+        XCTAssertFalse(todoItem!.isDone)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testToCsv() {
+        let todoItem = TodoItem(text: "Do homework 1 in SHMR", importance: .important, isDone: false)
+        let csv = todoItem.csv
+        
+        XCTAssertNotNil(csv)
+        XCTAssertTrue(csv.contains("Do homework 1 in SHMR"))
+        XCTAssertTrue(csv.contains("important"))
+        XCTAssertTrue(csv.contains("false"))
     }
+
+    func testParseJsonWithMissingFields() {
+        let json: [String: Any] = ["id": "1",
+                                   "importance": "important",
+                                   "isDone": false]
+
+        let todoItem = TodoItem.parse(json: json)
+        
+        XCTAssertNil(todoItem)
+    }
+
+    func testParseCsvWithMissingFields() {
+        let csv = "1,,false,important"
+
+        let todoItem = TodoItem.parse(csv: csv)
+        
+        XCTAssertNil(todoItem)
+    }
+
+    func testToJsonWithRegularImportance() {
+        let todoItem = TodoItem(text: "Do homework 1 in SHMR", importance: .regular, isDone: false)
+        let json = todoItem.json as? [String: Any]
+        
+        XCTAssertNotNil(json)
+        XCTAssertNil(json?["importance"])
+    }
+
+    func testToCsvWithRegularImportance() {
+        let todoItem = TodoItem(text: "Do homework 1 in SHMR", importance: .regular, isDone: false)
+        let csv = todoItem.csv
+        
+        XCTAssertNotNil(csv)
+        XCTAssertTrue(csv.contains(","))
+    }
+
+    static var allTests = [
+        ("testInitTodoItem", testInitTodoItem),
+        ("testParseJson", testParseJson),
+        ("testToJson", testToJson),
+        ("testParseCsv", testParseCsv),
+        ("testToCsv", testToCsv),
+        ("testParseJsonWithMissingFields", testParseJsonWithMissingFields),
+        ("testParseCsvWithMissingFields", testParseCsvWithMissingFields),
+        ("testToJsonWithRegularImportance", testToJsonWithRegularImportance),
+        ("testToCsvWithRegularImportance", testToCsvWithRegularImportance),
+    ]
 
 }
