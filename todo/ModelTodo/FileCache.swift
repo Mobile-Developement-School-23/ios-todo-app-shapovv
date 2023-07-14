0dab6b25afd5f083f58f5952e66f7bcf6a9f7d82
shapovv <asshapovalov@yandex.ru>
2023-07-15 00:17:10 +0300

    added SQLite

--- todo/ModelTodo/FileCache.swift
+++ todo/ModelTodo/FileCache.swift
@@ -1,6 +1,7 @@
 import Foundation
 import CoreData
 import UIKit
+import SQLite3
 
 let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
@@ -31,71 +32,173 @@ class FileCache {
     private func getDirectoryURL(for filename: String, withExtension ext: String) -> URL {
         return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(filename + "." + ext)
     }
+    
+    // MARK: Normal saveToFile and loadFromFile
 
-//    func saveToFile(filename: String) throws {
-//        let directoryURL = getDirectoryURL(for: filename, withExtension: "json")
-//        let jsonItems = items.map({$0.json})
-//        let data = try JSONSerialization.data(withJSONObject: jsonItems, options: .prettyPrinted)
-//        try data.write(to: directoryURL)
-//    }
-    func saveToFile() throws {
-        print("Using CoreData")
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Model")
-        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+    //    func saveToFile(filename: String) throws {
+    //        let directoryURL = getDirectoryURL(for: filename, withExtension: "json")
+    //        let jsonItems = items.map({$0.json})
+    //        let data = try JSONSerialization.data(withJSONObject: jsonItems, options: .prettyPrinted)
+    //        try data.write(to: directoryURL)
+    //    }
 
-        try context.execute(deleteRequest)
+    //    func loadFromFile(filename: String) throws {
+    //        let directoryURL = getDirectoryURL(for: filename, withExtension: "json")
+    //
+    //        guard FileManager.default.fileExists(atPath: directoryURL.path) else {
+    //            throw FileCacheError.fileNotFound
+    //        }
+    //
+    //        let data = try Data(contentsOf: directoryURL)
+    //
+    //        guard let jsonItems = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+    //            throw FileCacheError.dataParsingFailed
+    //        }
+    //
+    //        items = jsonItems.compactMap({TodoItem.parse(json: $0)})
+    //    }
+
+    // MARK: saveToFile with CoreData
+
+    //    func saveToFile() throws {
+    //        print("Using CoreData for Save")
+    //        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Model")
+    //        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+    //
+    //        try context.execute(deleteRequest)
+    //
+    //        for item in items {
+    //            let entity = NSEntityDescription.entity(forEntityName: "Model", in: context)
+    //            let newItem = NSManagedObject(entity: entity!, insertInto: context) as! Model
+    //            newItem.id = item.id
+    //            newItem.text = item.text
+    //            newItem.isDone = item.isDone
+    //            newItem.creationDate = item.creationDate
+    //            newItem.importance = item.importance.rawValue
+    //            newItem.deadline = item.deadline
+    //            newItem.modificationDate = item.modificationDate
+    //
+    //            try context.save()
+    //
+    //        }
+    //        let path = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).first
+    //
+    //    }
+    //
+    //    func loadFromFile() throws {
+    //        print("Using CoreData for Load")
+    //        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Model")
+    //
+    //        do {
+    //            let fetchedResults = try context.fetch(fetchRequest)
+    //            for result in fetchedResults {
+    //                guard let todoItem = result as? Model else {
+    //                    continue
+    //                }
+    //                let item = TodoItem(id: todoItem.id!, text: todoItem.text!, importance: Importance(rawValue: todoItem.importance!)!, deadline: todoItem.deadline, isDone: todoItem.isDone, creationDate: todoItem.creationDate!, modificationDate: todoItem.modificationDate)
+    //                items.append(item)
+    //            }
+    //        } catch let error as NSError {
+    //            print("Не удалось получить данные. \(error), \(error.userInfo)")
+    //        }
+    //    }
+    //
+    //    func loadFromFile(filename: String) throws {
+    //        let directoryURL = getDirectoryURL(for: filename, withExtension: "json")
+    //
+    //        guard FileManager.default.fileExists(atPath: directoryURL.path) else {
+    //            throw FileCacheError.fileNotFound
+    //        }
+    //
+    //        let data = try Data(contentsOf: directoryURL)
+    //
+    //        guard let jsonItems = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+    //            throw FileCacheError.dataParsingFailed
+    //        }
+    //
+    //        items = jsonItems.compactMap({TodoItem.parse(json: $0)})
+    //    }
+
+    // MARK: saveToFile with SQLite
+
+    func saveToFile() {
+        print("Using SQLite for Save")
+        var db: OpaquePointer?
+        var stmt: OpaquePointer?
+
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("ItemsDatabase.sqlite")
+
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+
+        if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Items (id TEXT PRIMARY KEY, text TEXT, importance TEXT, deadline TEXT, isDone INTEGER, creationDate TEXT, modificationDate TEXT)", nil, nil, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error creating table: \(errmsg)")
+        }
 
         for item in items {
-            let entity = NSEntityDescription.entity(forEntityName: "Model", in: context)
-            let newItem = NSManagedObject(entity: entity!, insertInto: context) as! Model
-            newItem.id = item.id
-            newItem.text = item.text
-            newItem.isDone = item.isDone
-            newItem.creationDate = item.creationDate
-            newItem.importance = item.importance.rawValue
-            newItem.deadline = item.deadline
-            newItem.modificationDate = item.modificationDate
+            let insertStatement = "INSERT INTO Items (id, text, importance, deadline, isDone, creationDate, modificationDate) VALUES (?, ?, ?, ?, ?, ?, ?);"
+            if sqlite3_prepare_v2(db, insertStatement, -1, &stmt, nil) == SQLITE_OK {
+                sqlite3_bind_text(stmt, 1, NSString(string: item.id).utf8String, -1, nil)
+                sqlite3_bind_text(stmt, 2, NSString(string: item.text).utf8String, -1, nil)
+                sqlite3_bind_text(stmt, 3, NSString(string: item.importance.rawValue).utf8String, -1, nil)
+                sqlite3_bind_text(stmt, 4, NSString(string: "\(item.deadline ?? Date())").utf8String, -1, nil)
+                sqlite3_bind_int(stmt, 5, item.isDone ? 1 : 0)
+                sqlite3_bind_text(stmt, 6, NSString(string: "\(item.creationDate)").utf8String, -1, nil)
+                sqlite3_bind_text(stmt, 7, NSString(string: "\(item.modificationDate ?? Date())").utf8String, -1, nil)
 
-            try context.save()
+                if sqlite3_step(stmt) != SQLITE_DONE {
+                    let errmsg = String(cString: sqlite3_errmsg(db)!)
+                    print("failure inserting item: \(errmsg)")
+                }
 
+            } else {
+                print("INSERT statement could not be prepared.")
+            }
+            sqlite3_finalize(stmt)
         }
-        let path = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).first
-//        print(path)
+
+        sqlite3_close(db)
     }
 
+    func loadFromFile() {
+        print("Using SQLite for Load")
+        var db: OpaquePointer?
+        var stmt: OpaquePointer?
 
-    func loadFromFile() throws {
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Model")
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("ItemsDatabase.sqlite")
 
-        do {
-            let fetchedResults = try context.fetch(fetchRequest)
-            for result in fetchedResults {
-                guard let todoItem = result as? Model else {
-                    continue
-                }
-                let item = TodoItem(id: todoItem.id!, text: todoItem.text!, importance: Importance(rawValue: todoItem.importance!)!, deadline: todoItem.deadline, isDone: todoItem.isDone, creationDate: todoItem.creationDate!, modificationDate: todoItem.modificationDate)
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+
+        let queryString = "SELECT * FROM Items;"
+        if sqlite3_prepare_v2(db, queryString, -1, &stmt, nil) == SQLITE_OK {
+            while sqlite3_step(stmt) == SQLITE_ROW {
+                let id = String(cString: sqlite3_column_text(stmt, 0))
+                let text = String(cString: sqlite3_column_text(stmt, 1))
+                let importance = Importance(rawValue: String(cString: sqlite3_column_text(stmt, 2)))!
+                let deadline = Date()
+                let isDone = sqlite3_column_int(stmt, 4) != 0
+                let creationDate = Date()
+                let modificationDate = Date()
+
+                let item = TodoItem(id: id, text: text, importance: importance, deadline: deadline, isDone: isDone, creationDate: creationDate, modificationDate: modificationDate)
+
                 items.append(item)
             }
-        } catch let error as NSError {
-            print("Не удалось получить данные. \(error), \(error.userInfo)")
+        } else {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing select: \(errmsg)")
         }
+
+        sqlite3_close(db)
     }
 
-    func loadFromFile(filename: String) throws {
-        let directoryURL = getDirectoryURL(for: filename, withExtension: "json")
-
-        guard FileManager.default.fileExists(atPath: directoryURL.path) else {
-            throw FileCacheError.fileNotFound
-        }
-
-        let data = try Data(contentsOf: directoryURL)
-
-        guard let jsonItems = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
-            throw FileCacheError.dataParsingFailed
-        }
-
-        items = jsonItems.compactMap({TodoItem.parse(json: $0)})
-    }
+    // MARK: CSV
 
     func saveToFileAsCSV(filename: String) throws {
         let directoryURL = getDirectoryURL(for: filename, withExtension: "csv")
